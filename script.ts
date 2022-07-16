15fdb76f6a98dc6ea5772afdb3105f8d83a4ba1a
Hejo <59996938+DevHejo@users.noreply.github.com>
2022-07-16 13:47:31 +0530

    Migrations made
    The models where created and the database was reset, then migrated once more

--- script.ts
+++ script.ts
@@ -1,13 +1,39 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
+// async function main() {
+//     // prisma client queries
+//     // const user = await prisma.user.create({ data: { name: "kyle" } });
+//     // const user = await prisma.user.create({ data: { name: "sally" } });
+//     // const user = await prisma.user.create({ data: { name: "jhonothen" } });
+//     // const user = await prisma.user.findMany();
+//     await prisma.user.deleteMany();
+//     // console.log(user);
+// }
+
 async function main() {
-    // prisma client queries
-    // const user = await prisma.user.create({ data: { name: "kyle" } });
-    // const user = await prisma.user.create({ data: { name: "sally" } });
-    // const user = await prisma.user.create({ data: { name: "jhonothen" } });
-    const user = await prisma.user.findMany();
+    // delete all the previous users. since saving each time now is runnig the app and trying to add data into the db.
+    await prisma.user
+        .deleteMany()
+        .then(() => {
+            console.log("\nAll users deleted\n");
+        })
+        .catch((e) => {
+            console.log("Error while deleting users");
+            console.error(e);
+        });
+
+    // create a user
+    const user = await prisma.user.create({
+        data: {
+            name: "kyle",
+            email: "kyle@test.com",
+            age: 27,
+        },
+    });
+
     console.log(user);
+    console.log("\n");
 }
 
 main()
